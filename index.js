7987b17df18151e75bad988674c93d526cb7fd1e
Cecilia urrutia <urrutia.cici@gmail.com>
2024-08-28 16:01:47 -0300

    add url digital

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(cors({
     origin: function (origin, callback) {
-        const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000/digitalbook'];
+        const allowedOrigins = ['http://localhost:5173', 'https://bookstorebackend-digital.vercel.app/digitalbook'];
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
